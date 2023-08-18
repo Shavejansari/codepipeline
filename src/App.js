a0ed82e76ce494117be00f71a0c95f67370e5694
Shavej <shavejmohd30@gmail.com>
2023-08-18 15:48:24 +0530

    name adding

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> YoYo :-)
+          Welcome to Mohd Shavej tera baap :-)
         </p>
         <a
           className="App-link"
