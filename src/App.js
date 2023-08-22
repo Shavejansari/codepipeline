20f87d434cb41e08d2589836f0a1afc6c4a5dafb
Shavej Ansari <shavejmohd30@gmail.com>
2023-08-22 08:20:29 +0000

    adding

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to  Mohd Shavej Blog  how may i help you :-)
+          Welcome to Mohd Shavej Blog  how may i help you :-)
         </p>
         <a
           className="App-link"
